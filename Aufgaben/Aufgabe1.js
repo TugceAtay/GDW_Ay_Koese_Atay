13df5602d2786d95f97adec950e0800e9de411a0
TugceAtay <tugceatay95@gmail.com>
2020-11-12 03:47:07 +0100

    Struktur der Übungsblätter

--- Aufgaben/Aufgabe1.js
+++ Aufgaben/Aufgabe1.js
@@ -1,2 +1,20 @@
 console.log("Übungsblatt 1");
 
+//maximal Höhe der Bewertung
+const maxHoehe = 100;
+
+
+let aktuell = 0;
+let eingabe=0;
+let gesamtAnzahl = 0;
+
+// Ausgaben:
+console.log('Die Höchstmögliche Gesamtbewertung beträgt '+maxHoehe+ ' Sterne.');
+console.log("Die aktuelle Anzahl der Bewertung ist bei "+aktuell +" Sternen");
+console.log("Eingabe : " +eingabe+ ' Sterne\n');
+console.log("Nach veränderung: \n")
+aktuell = 1
+eingabe = 2.5
+
+// Ausgabe nach veränderung
+console.log("Die aktuelle Anzahl der Bewertung ist bei " +aktuell +" Sternen");
